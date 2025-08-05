9a0de1ad8d947df204217020655d55e4e57ec1c9
Carlos Henrique Guardão Gandarez <gandarez@gmail.com>
2025-08-05 12:45:34 -0300

    Add main routine

--- cmd/service/main.go
+++ cmd/service/main.go
@@ -2,18 +2,25 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 
+	"github.com/gandarez/btc-price-service/internal/app/domain/checkapp"
+	"github.com/gandarez/btc-price-service/internal/app/domain/priceapp"
+	"github.com/gandarez/btc-price-service/internal/app/sdk/mux"
+	"github.com/gandarez/btc-price-service/internal/business/domain/pricebus"
+	"github.com/gandarez/btc-price-service/internal/business/sdk/coindeskclient"
+	"github.com/gandarez/btc-price-service/internal/foundation/config"
 	"github.com/gandarez/btc-price-service/internal/foundation/log"
 	"github.com/gandarez/btc-price-service/internal/foundation/version"
-)
-
-const (
-	// serviceName is the name of the service.
-	serviceName = "btc-price-service"
+	"github.com/gandarez/btc-price-service/internal/foundation/web"
 )
 
 func main() {
@@ -21,14 +28,88 @@ func main() {
 
 	// Initialize logger
 	logger := log.New(os.Stdout)
+
+	cfg, err := config.Load("./configs/.env")
+	if err != nil {
+		logger.Fatalf("failed to load config: %v", err)
+	}
+
 	logger.WithFields([]zapcore.Field{
-		zap.String("service", serviceName),
+		zap.String("service", cfg.ServiceName),
 		zap.String("version", version.Version),
-		zap.String("environment", "development"),
+		zap.String("environment", cfg.Environment),
 	}...)
 
 	// Save logger to context
 	ctx = log.ToContext(ctx, logger)
 
-	logger.Infof("Service %s is starting", serviceName)
+	logger.Infof("service %s is starting..", cfg.ServiceName)
+	logger.Infof("params: %s", cfg.String())
+
+	// Initialize price bus
+	coindeskcli := coindeskclient.NewClient(
+		cfg.CoinDeskConfig.URL,
+		cfg.CoinDeskConfig.APIKey,
+	)
+	priceBus := pricebus.NewBusiness(coindeskcli)
+
+	// build http routes
+	cfgMux := mux.Config{
+		PriceConfig: mux.PriceConfig{
+			BufferTTL:                 time.Duration(cfg.CacheConfig.TTL) * time.Second,
+			MaxCacheSize:              cfg.CacheConfig.MaxSize,
+			DefaultExpirationInterval: time.Duration(cfg.CacheConfig.ExpirationInterval) * time.Second,
+			PollInterval:              time.Duration(cfg.CoinDeskConfig.PollInterval) * time.Second,
+			MaxPeersPerBroadcaster:    cfg.BroadcastConfig.MaxPeersPerBroadcaster,
+			PriceBus:                  priceBus,
+		},
+	}
+
+	mux := mux.WebAPI(ctx, cfgMux, buildRoutes())
+	server := &http.Server{
+		Addr:              fmt.Sprintf(":%d", cfg.ServerConfig.Port),
+		ReadHeaderTimeout: time.Duration(cfg.ServerConfig.ReadHeaderTimeout) * time.Second,
+		Handler:           mux,
+	}
+
+	// Start http server
+	serverError := make(chan error, 1)
+
+	go func() {
+		logger.Infof("http server started on %s", server.Addr)
+
+		serverError <- server.ListenAndServe()
+	}()
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
+
+	select {
+	case err := <-serverError:
+		logger.Fatalf("server error: %v", err)
+	case <-shutdown:
+		logger.Infoln("received shutdown signal, shutting down service")
+
+		ctx, cancel := context.WithTimeout(ctx, time.Duration(cfg.ShutdownTimeout)*time.Second)
+		defer cancel()
+
+		if err := server.Shutdown(ctx); err != nil {
+			logger.Errorf("failed to shutdown server: %v", err)
+		}
+
+		logger.Infof("service %s has shut down", cfg.ServiceName)
+	}
+
+	logger.Infof("service %s gracefully stopped", cfg.ServiceName)
+}
+
+type add struct{}
+
+func buildRoutes() mux.RouteAdder {
+	return add{}
+}
+
+func (add) Add(ctx context.Context, app *web.App, cfg mux.Config) {
+	checkapp.Routes(ctx, app)
+	priceapp.Routes(ctx, app, cfg)
 }
